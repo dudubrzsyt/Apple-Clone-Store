fd443d29146d94b9e3317c881f46cfe05403182b
igor eduardo pinheiro <dudubrzsyt13@gmail.com>
2026-02-02 09:11:34 -0300

    Corrige vite.config.ts unificando configuração do server

--- apple.com/vite.config.ts
+++ apple.com/vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 // https://vite.dev/config/
-  export default defineConfig({
+export default defineConfig({
   plugins: [react()],
+
   server: {
     host: true,   // força expor na rede local
     port: 5173,   // porta padrão
-  },
-});
+    fs: {
+      deny: ['backups'] // impede acesso à pasta backups
+    }
+  }
+})
